088246c545d9e0b0a91aee6627e9db89332f07fc
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-01-30 16:00:33 +0000

    Levi - Interactable Doors

---
+++ Don'tTurn/Assets/Scripts/Interactables/Interactables.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+    private Animator animator;
+    public bool isActive;
+    public bool isPressedOnce; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.E) && isActive == true)
+        {
+            if(isPressedOnce == false)
+            {
+                Pressed();
+            }
+            else if(isPressedOnce == true)
+            {
+                UnPressed(); 
+            }
+            
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            isActive = true;
+            Debug.Log("PRESS E!"); 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isActive = false;
+        animator.SetBool("isActive", false); 
+    }
+
+    public void Pressed()
+    {
+        animator.SetBool("isActive", true);
+        isPressedOnce = true;
+    }
+
+    public void UnPressed()
+    {
+        animator.SetBool("isActive", false);
+        isPressedOnce = false; 
+    }
+}
