f738968dd53f416551ea9e1c88cf01a05a1024d2
Harvey Fleming <94860455+SneakyDevil77@users.noreply.github.com>
2023-01-31 16:01:41 +0000

    Basic Melee Attack Implemented

--- Don'tTurn/Assets/Scripts/Player/AttackScript.cs
+++ Don'tTurn/Assets/Scripts/Player/AttackScript.cs
@@ -4,24 +4,70 @@ using UnityEngine;
 
 public class AttackScript : MonoBehaviour
 {
+
+    [Header("Melee Attack Stats")]
+
+    [SerializeField] private Transform attackCirclePoint;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private int attackDamage;
+    [SerializeField]private float attackCooldownTime = 1f;
+        
+    private bool isTimerDone = true;
+    private float timeToNextAttack;
+
+    [Header("Corruption Effect")]
+    [SerializeField] private CorruptionScript corruptionScript;
+    private int attackSpeedMultiplier;
+
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeToNextAttack = attackCooldownTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(Keycode.P)) 
-       {
-            MeleeAttack();
-       }
+        MeleeAttackCooldown();
     }
 
-    private void MeleeAttack()
+    private void MeleeAttackCooldown()
     {
+        if (isTimerDone)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                MeleeAttack();
+                isTimerDone = false;
+            }
+        }
+        else
+        {
+            timeToNextAttack -= 1 * Time.deltaTime;
+            if (timeToNextAttack <= 0)
+            {
+                isTimerDone = true;
+                timeToNextAttack = attackCooldownTime;
+                return;
+            }
+        }
+    }
 
+    void MeleeAttack()
+    {
+        Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackCirclePoint.position, attackRadius);
+
+        foreach (Collider2D Enemy in enemiesHit)
+        {
+            Enemy.GetComponent<EnemyStats>().OnHit(attackDamage);
+        } 
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.DrawWireSphere(attackCirclePoint.position, attackRadius);
     }
 }
--- Don'tTurn/Assets/Scripts/Player/PlayerStats.cs
+++ Don'tTurn/Assets/Scripts/Player/PlayerStats.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats : MonoBehaviour, IDataPersistence
 {
     public float health = 100f;
+    private Vector3 playerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +30,15 @@ public class PlayerStats : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
+    }
+
+    public void LoadData(GameData data)
+    {
+        this.transform.position = data.playerPosition;
+    }
+
+    public void SaveData(GameData data)
+    {
+        data.playerPosition = this.transform.position;
     }
 }
