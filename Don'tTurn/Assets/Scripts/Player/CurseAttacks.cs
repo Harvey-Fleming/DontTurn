6fe3297a518f8e956622fef34f62b6ada6cb2d3c
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-02-13 00:16:02 +0000

    Started making the bomb, need help with the flipping of the player in the movement and also the explosion part

---
+++ Don'tTurn/Assets/Scripts/Player/CurseAttacks.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurseAttacks : MonoBehaviour
+{
+    public GameObject bomb;
+    public Transform firePoint; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot(); 
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bomb, firePoint.position, firePoint.rotation); 
+    }
+}
