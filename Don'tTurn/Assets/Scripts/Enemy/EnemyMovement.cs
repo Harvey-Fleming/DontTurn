f9cea5412ae3fbb62f7db6f4d52e4fb01729f031
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-02-20 14:46:51 +0000

    Commit

---
+++ Don'tTurn/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Movement Info")]
+    public int moveDirection;
+    public float moveSpeed;
+    public float damageRange;
+    public float maxRange;
+    public float wanderDistance;
+    public float jumpForce;
+    float jumpTimer;
+    public float jumpCooldown;
+
+    Rigidbody2D rb;
+    Transform player;
+    RaycastHit2D hit;
+    public LayerMask layerMask;
+    public LayerMask jumpMask;
+
+    [Header("States")]
+    public bool isWandering;
+    public bool isAggro;
+    public bool isDamaging;
+    public bool canJump;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+
+        isWandering = true;
+
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(WanderDelay());
+    }
+
+    private void Update()
+    {
+        if (!isAggro)
+        {
+            isWandering = true;
+        }
+
+        if (!canJump)
+        {
+            jumpTimer += Time.deltaTime;
+
+            if (jumpTimer >= jumpCooldown)
+            {
+                canJump = true;
+            }
+        }
+
+        if (Vector2.Distance(transform.position, player.position) < maxRange)
+        {
+            hit = Physics2D.Raycast(transform.position, (player.position - transform.position), maxRange, ~layerMask);
+
+            if (hit.transform == player)
+            {
+                isAggro = true;
+                isWandering = false;
+            }
+            else
+            {
+                isAggro = false;
+            }
+        }
+        else
+        {
+            isAggro = false;
+        }
+
+        if (isAggro)
+        {
+            AggroMovement();
+        }
+
+        if (isWandering)
+        {
+            WanderMovement();
+        }
+    }
+
+    void WanderMovement()
+    {
+        if (Vector2.Distance(transform.position, player.position) < wanderDistance)
+        {
+            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+            moveSpeed = 2f;
+
+            if (Physics2D.Raycast(transform.position, new Vector2(moveDirection , 0), 1, jumpMask))
+            {
+                if (canJump)
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                    canJump = false;
+                    jumpTimer = 0;
+                }
+            }
+        }
+    }
+
+    IEnumerator WanderDelay()
+    {
+        while (true)
+        {
+            if (Random.Range(0, 2) == 0)
+            {
+                moveDirection = -1;
+            }
+            else
+            {
+                moveDirection = 1;
+            }
+
+            yield return new WaitForSeconds(Random.Range(0.5f,1.5f));
+            moveDirection = 0;
+            yield return new WaitForSeconds(Random.Range(1.5f, 2.5f));
+        }
+    }
+
+    void AggroMovement()
+    {
+        if (!isDamaging)
+        {
+            if (Vector2.Distance(transform.position, player.position) > damageRange)
+            {
+                if (player.position.x > transform.position.x)
+                {
+                    moveDirection = 1;
+
+                    if(Physics2D.Raycast(transform.position, Vector2.right, 2, jumpMask))
+                    {
+                        if (canJump)
+                        {
+                            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                            canJump = false;
+                            jumpTimer = 0;
+                        }
+                    }
+                }
+                if (player.position.x < transform.position.x)
+                {
+                    moveDirection = -1;
+
+                    if (Physics2D.Raycast(transform.position, Vector2.left, 2, jumpMask))
+                    {
+                        if (canJump)
+                        {
+                            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                            canJump = false;
+                            jumpTimer = 0;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                StartCoroutine(Damage());
+            }
+        }
+
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        moveSpeed = 4f;
+    }
+
+    IEnumerator Damage()
+    {
+        //hit
+
+        isDamaging = true;
+        moveDirection = 0;
+        yield return new WaitForSeconds(1f);
+        isDamaging = false;
+
+    }
+}
--- Don'tTurn/Assets/Scripts/Interactables/UnlockAbility.cs
+++ Don'tTurn/Assets/Scripts/Interactables/UnlockAbility.cs
@@ -1,26 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UnlockAbility : MonoBehaviour
 {
     [SerializeField] private string NPCAbility;
+    [SerializeField] private GameObject playerObj;
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void Awake() 
     {
-        if (other.gameObject.tag == "Player")
+        playerObj = GameObject.FindWithTag("Player");
+    }
+
+    private void OnAbilityUnlock() 
+    {
+        if (playerObj != null)
         {
             switch(NPCAbility)
             {
                 case("Dash"):
-                other.gameObject.GetComponent<PrototypeDash>().isUnlocked = true;
+                playerObj.GetComponent<PrototypeDash>().isUnlocked = true;
                 break;
                 case("Double Jump"):
-                other.gameObject.GetComponent<PlayerMovement>().maxAerialJumpCount = 1;
+                playerObj.GetComponent<PlayerMovement>().maxAerialJumpCount = 1;
+                break;
+                case("GrappleHook"):
+                playerObj.GetComponent<GrappleAbility>().isUnlocked = true;
+                break;
+                default:
                 break;
             }
         }
         
     }
+
+    private void OnEnable() 
+    {
+        DialogueTrigger.OnDialogueEnd += OnAbilityUnlock;
+    }
+
+    private void OnDisable() 
+    {
+        DialogueTrigger.OnDialogueEnd -= OnAbilityUnlock;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (playerObj == null)
+        {
+            playerObj = GameObject.FindWithTag("Player");
+        }
+    }
+
+    public void OnSceneUnloaded(Scene scene)
+    {
+        playerObj = null;
+    }
+
 }
 
