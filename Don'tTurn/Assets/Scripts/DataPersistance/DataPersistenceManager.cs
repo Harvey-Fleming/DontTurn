98a9de3a7341b512f7bae223f784786babb3220d
Harvey Fleming <94860455+SneakyDevil77@users.noreply.github.com>
2023-02-26 22:50:23 +0000

    Bug Fixes
    Jump animation bug when punching in air  + Health now resets when returning to main menu and selecting new game.

--- Don'tTurn/Assets/Scripts/DataPersistance/DataPersistenceManager.cs
+++ Don'tTurn/Assets/Scripts/DataPersistance/DataPersistenceManager.cs
@@ -51,17 +51,17 @@ public class DataPersistenceManager : MonoBehaviour
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         this.dataPersistenceObjects = FindAllDataPersistenceObjects();
-        LoadGame();
     }
 
     public void OnSceneUnloaded(Scene scene)
     {
-        SaveGame();
+
     }
 
     public void NewGame()
     {
         this.gameData = new GameData();
+
     }
 
     public void LoadGame()
@@ -104,6 +104,7 @@ public class DataPersistenceManager : MonoBehaviour
         
         //save that data to a file using the data handler
         dataHandler.Save(gameData);
+       Debug.Log("Saved Game");
     }
 
     public void OnCheckPointReached() 
