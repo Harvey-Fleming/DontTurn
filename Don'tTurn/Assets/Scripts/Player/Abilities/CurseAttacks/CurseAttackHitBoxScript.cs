0fe40b57de98258e5cb046f6775b4c7ecbc1f6fe
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-02-27 15:07:28 +0000

    EATING ENEMY :d

--- Don'tTurn/Assets/Scripts/Player/Abilities/CurseAttacks/CurseAttackHitBoxScript.cs
+++ Don'tTurn/Assets/Scripts/Player/Abilities/CurseAttacks/CurseAttackHitBoxScript.cs
@@ -8,6 +8,7 @@ public class CurseAttackHitBoxScript : MonoBehaviour
     public string attackType;
     //2 attack types "Eat" and "Punch"
    [SerializeField] GameObject enemy;
+    [SerializeField] Rigidbody2D enemyRigidBody; 
     public GameObject player;
     bool isColliding = false; 
     // Start is called before the first frame update
@@ -24,29 +25,53 @@ public class CurseAttackHitBoxScript : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if(collision.gameObject.CompareTag("Enemy") && isColliding == false)
+    //    {
+    //        isColliding = true; 
+    //        enemy = collision.gameObject;
+
+    //        switch (attackType)
+    //        {
+    //            case "Eat":
+    //                Debug.Log("Entering Coroutine"); 
+    //                StartCoroutine(EatEnemy());
+    //                break;
+    //            case "Punch":
+    //                PunchEnemy(); 
+    //                break; 
+    //        }
+    //    }
+    //    else
+    //    {
+    //        isColliding = false; 
+    //    }
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Enemy") && isColliding == false)
+        if (collision.gameObject.CompareTag("Enemy") && isColliding == false)
         {
-            isColliding = true; 
+            isColliding = true;
             enemy = collision.gameObject;
 
             switch (attackType)
             {
                 case "Eat":
-                    Debug.Log("Entering Coroutine"); 
+                    Debug.Log("Entering Coroutine");
                     StartCoroutine(EatEnemy());
                     break;
                 case "Punch":
-                    PunchEnemy(); 
-                    break; 
+                    PunchEnemy();
+                    break;
             }
         }
         else
         {
-            isColliding = false; 
+            isColliding = false;
         }
-        
     }
 
     public IEnumerator EatEnemy()
@@ -56,26 +81,25 @@ public class CurseAttackHitBoxScript : MonoBehaviour
         if(isColliding == true)
         {
             enemy.transform.position = player.transform.position;
-            enemy.transform.position += new Vector3(2f, 0f, 0f);
+            enemy.transform.position += new Vector3(1f, 0f, 0f);
             enemy.GetComponent<SpriteRenderer>().enabled = false;
             enemy.GetComponent<BoxCollider2D>().enabled = false;
-
-            yield return new WaitForSeconds(0.2f);
-
+            enemyRigidBody = enemy.GetComponent<Rigidbody2D>(); 
+            enemyRigidBody.constraints = RigidbodyConstraints2D.FreezePositionY;
+            Debug.Log("Waiting 0.1 second");
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log("Time has gone"); 
             if (random == 0)
             {
                 Debug.Log("Will Spit out");
                 enemy.GetComponent<BoxCollider2D>().enabled = true;
+                enemyRigidBody.constraints = RigidbodyConstraints2D.None;
+                enemyRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
                 enemy.GetComponent<SpriteRenderer>().enabled = true;
                 //enemy.GetComponent<Rigidbody2D>().velocity = transform.right * 5f;
-                yield return new WaitForSeconds(0.5f);
+                enemy.GetComponent<Rigidbody2D>().velocity = transform.right * 5f;
+                //Enemy on hit Function :D
                 //enemy.GetComponent<Rigidbody2D>().velocity = transform.right * 0f;
-                enemy = null;
-            }
-            else if (random == 2)
-            {
-                Debug.Log("Killed");
-                Object.Destroy(enemy);
                 enemy = null;
             }
 
