0c37196a502312aea223770719a3f25a0d7e9deb
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-04-26 19:22:08 +0100

    Revert "Curse tutorials almost finished"
    This reverts commit 6173d2560349670f374488121741b164bdf90fb8.

--- Don'tTurn/Assets/Scripts/UI/CurseTutorialPanel.cs
+++ Don'tTurn/Assets/Scripts/UI/CurseTutorialPanel.cs
@@ -8,13 +8,12 @@ public class CurseTutorialPanel : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public VideoClip[] videoClips;
-    public int tutorialNumber;
-    public GameObject videoPlayerMaterial; 
+    int tutorialNumber; 
 
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayerMaterial.SetActive(true);
+        
     }
 
     // Update is called once per frame
@@ -41,13 +40,5 @@ public class CurseTutorialPanel : MonoBehaviour
                 videoPlayer.clip = videoClips[5];
                 break;
         }
-    }
-
-
-    public void close()
-    {
-        Time.timeScale = 1;
-        videoPlayerMaterial.SetActive(false); 
-        this.gameObject.SetActive(false); 
     }
 }
