b38d6af1dc5438d2f6d9abf9a790ab7809d80d6b
SergeantSkylight <michael42hennessy@gmail.com>
2023-06-01 15:57:11 +0100

    player icon last checkpoint on map implementation

--- Don'tTurn/Assets/Scripts/UI/MapPanelScript.cs
+++ Don'tTurn/Assets/Scripts/UI/MapPanelScript.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class MapPanelScript : MonoBehaviour
 {
     public Image[] images; 
-    public MapScript MapScript;  
+    public MapScript MapScript;
+    public GameObject icon;
+    public Vector3[] iconLoc;
 
     // Start is called before the first frame update
     void Start()
     {
+        icon.SetActive(false);
         for (int i = 0; i < images.Length; i++)
         {
             images[i].gameObject.SetActive(false);
@@ -19,6 +22,8 @@ public class MapPanelScript : MonoBehaviour
 
     public void ShowMap(int mapNum)
     {
+        icon.SetActive(true);
         images[mapNum - 1].gameObject.SetActive(true);
+        icon.transform.localPosition = iconLoc[mapNum - 1];
     }
 }
